416c8361c47dfa9a9a32be3e27a2d145b4c987ec
Alexandre Zani <alexandre.zani@gmail.com>
2010-11-08 22:31:44 -0800

    Created basic requests for all individuals, all museum objects and single of either

---
+++ src/py/hsr_server/requests.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+#   Copyright Alexandre Zani (alexandre.zani@gmail.com) 
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+def abstract():
+  import inspect
+  caller = inspect.getouterframes(inspect.currentframe())[1][3]
+  raise NotImplementedError(caller + ' must be implemented in subclass') 
+
+request_types = {
+    "Ping" : "PingRequest",
+    "ListMuseumObjects" : "AllObjectsRequest",
+    "GetMuseumObject" : "GetMuseumObjectRequest",
+    "ListIndividuals" : "AllIndividualsRequest",
+    "GetBiologicalIndividual" : "GetBioIndividualRequest"
+    }
+
+class HSRRequestException(Exception): pass
+
+def getHSRRequest(method, args, credentials, hsr_db):
+  if method in request_types:
+    return globals()[request_types[method]](args, credentials, hsr_db)
+  raise HSRRequestException("Unknown request type " + method)
+
+class HSRRequest(object):
+  def execute(self): abstract()
+  def getRequestType(self): abstract()
+
+class PingRequest(HSRRequest):
+  def __init__(self, args, credentials, hsr_db):
+    self.credentials = credentials
+    self.hsr_db = hsr_db
+
+  def getRequestType(self):
+    return "Ping"
+
+  def execute(self):
+    self.credentials.getUserId()
+    cred_response = self.credentials.getResponse()
+    requ_response = "<response><type>Ping</type></response>"
+    return (requ_response, cred_response)
+
+class AllObjectsRequest(HSRRequest):
+  def __init__(self, args, credentials, hsr_db):
+    self.credentials = credentials
+    self.hsr_db = hsr_db
+
+  def getRequestType(self):
+    return "ListMuseumObjects"
+
+  def execute(self):
+    self.credentials.getUserId()
+    mos = self.hsr_db.getAllMuseumObjects()
+    mos_xml = "<response>"
+    for mo in mos:
+      mos_xml += mo.toXml()
+    mos_xml += "</response>"
+
+    return (mos_xml, self.credentials.getResponse())
+
+class GetMuseumObjectRequest(HSRRequest):
+  def __init__(self, args, credentials, hsr_db):
+    self.credentials = credentials
+    self.hsr_db = hsr_db
+    try:
+      self.object_id = args["object_id"]
+    except KeyError:
+      self.object_id = None
+
+  def getRequestType(self):
+    return "GetMuseumObject"
+
+  def execute(self):
+    self.credentials.getUserId()
+    cred_response = self.credentials.getResponse()
+    mo = self.hsr_db.getMuseumObjectById(self.object_id)
+    try:
+      mo_xml = "<response>" + mo.toXml() + "</response>"
+    except AttributeError:
+      mo_xml = "<response></response>"
+    return (mo_xml, cred_response)
+
+class AllIndividualsRequest(HSRRequest):
+  def __init__(self, args, credentials, hsr_db):
+    self.db = hsr_db
+    self.credentials = credentials
+
+  def getRequestType(self):
+    return "ListIndividuals"
+
+  def execute(self):
+    self.credentials.getUserId()
+    bis = self.db.getAllIndividuals()
+    bis_xml = "<response>"
+    for bi in bis:
+      bis_xml += bi.toXml()
+    bis_xml += "</response>"
+    return (bis_xml, self.credentials.getResponse())
+
+class GetBioIndividualRequest(HSRRequest):
+  def __init__(self, args, credentials, hsr_db):
+    self.credentials = credentials
+    self.hsr_db = hsr_db
+    try:
+      self.indiv_id = args["indiv_id"]
+    except KeyError:
+      self.indiv_id = None
+
+  def getRequestType(self):
+    return "GetBiologicalIndividual"
+
+  def execute(self):
+    self.credentials.getUserId()
+    bi = self.hsr_db.getIndividualById(self.indiv_id)
+    try:
+      response = "<response>" + bi.toXml() + "</response>"
+    except AttributeError:
+      response = "<response></response>"
+    return (response, self.credentials.getResponse())
+
+
+
+
---
+++ src/py/hsr_server/tests/test_requests.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+
+#   Copyright Alexandre Zani (alexandre.zani@gmail.com) 
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from hsr_server.requests import *
+from hsr_auth.credentials import HSRCredentialsException, HSRCredentials
+from hsr_server.tests.HSRDBTests import HSRDBTestImpl
+
+class MockCredentials(HSRCredentials):
+  def __init__(self, valid=True, user_id=None, response=""):
+    self.valid = valid
+    self.user_id = user_id
+    self.response = response
+
+  def getUserId(self):
+    if not self.valid:
+      raise HSRCredentialsException("InvalidCredentials")
+    return self.user_id
+
+  def getResponse(self):
+    return self.response
+
+class TestPingRequest:
+  def test_PingFactory(self):
+    request = getHSRRequest("Ping", {}, None, None)
+    assert request.getRequestType() == "Ping"
+
+  def test_PingExec(self):
+    request = getHSRRequest("Ping", {}, MockCredentials(True), None)
+    assert "<response><type>Ping</type></response>" == request.execute()[0]
+
+class TestListMuseumObjects:
+  def test_ListMuseumObjectsFactory(self):
+    request = getHSRRequest("ListMuseumObjects", {}, None, None)
+    assert request.getRequestType() == "ListMuseumObjects"
+
+  def test_ListMuseumObjects(self):
+    db = HSRDBTestImpl()
+    mo1 = db.newMuseumObject("hello", "hi", "berkeley")
+    mo2 = db.newMuseumObject("helslo", "hsfi", "berdkeley")
+
+    request = getHSRRequest("ListMuseumObjects", {}, MockCredentials(True), db)
+
+    response = request.execute()[0]
+
+    assert mo1.toXml() in response
+    assert mo2.toXml() in response
+
+  def test_ListMuseumObjectsInvalidCreds(self):
+    db = HSRDBTestImpl()
+    request = getHSRRequest("ListMuseumObjects", {}, MockCredentials(False), db)
+
+    try:
+      request.execute()
+    except HSRCredentialsException, (instance):
+      assert str(instance) == "InvalidCredentials"
+    else:
+      assert False
+
+class TestGetMuseumObjectRequest(HSRRequest):
+  def test_GetMuseumObjectRequestFactory(self):
+    request = getHSRRequest("GetMuseumObject", {}, None, None)
+    assert request.getRequestType() == "GetMuseumObject"
+
+  def test_GetMuseumObjectRequest(self):
+    db = HSRDBTestImpl()
+    mo = db.newMuseumObject("hello", "hi", "berkeley")
+    args = {"object_id" : mo.object_id}
+    credentials = MockCredentials(True)
+
+    request = getHSRRequest("GetMuseumObject", args, credentials, db)
+
+    response = request.execute()
+
+    assert mo.toXml() in response[0]
+    
+  def test_GetMuseumObjectRequestBadCreds(self):
+    db = HSRDBTestImpl()
+    mo = db.newMuseumObject("hello", "hi", "berkeley")
+    args = {"object_id" : mo.object_id}
+    credentials = MockCredentials(False)
+
+    request = getHSRRequest("GetMuseumObject", args, credentials, db)
+
+    try:
+      request.execute()
+    except HSRCredentialsException, (instance):
+      assert str(instance) == "InvalidCredentials"
+    else:
+      assert False
+
+  def test_GetMuseumObjectRequestMissingObjectId(self):
+    db = HSRDBTestImpl()
+    mo = db.newMuseumObject("hello", "hi", "berkeley")
+    args = {}
+    credentials = MockCredentials(True)
+
+    request = getHSRRequest("GetMuseumObject", args, credentials, db)
+
+    response = request.execute()
+
+    assert response[0] == "<response></response>"
+
+class TestListIndividualsRequest:
+  def test_ListIndividualsRequestFactory(self):
+    request = getHSRRequest("ListIndividuals", {}, None, None)
+    assert request.getRequestType() == "ListIndividuals"
+
+  def test_ListIndividuals(self):
+    db = HSRDBTestImpl()
+    bi1 = db.newIndividual("a", "a1", 10, 30, "NA", 1)
+    bi2 = db.newIndividual("b", "b1", 130, 310, "NA", 1)
+
+    args = {}
+    credentials = MockCredentials(True)
+
+    request = getHSRRequest("ListIndividuals", args, credentials, db)
+
+    response = request.execute()
+
+    assert bi1.toXml() in response[0]
+    assert bi2.toXml() in response[0]
+
+  def test_ListIndividualsBadCreds(self):
+    db = HSRDBTestImpl()
+    bi1 = db.newIndividual("a", "a1", 10, 30, "NA", 1)
+    bi2 = db.newIndividual("b", "b1", 130, 310, "NA", 1)
+
+    args = {}
+    credentials = MockCredentials(False)
+
+    request = getHSRRequest("ListIndividuals", args, credentials, db)
+
+    try:
+      response = request.execute()
+    except HSRCredentialsException, (instance):
+      str(instance) == "InvalidCredentials"
+    else:
+      assert False
+
+class TestGetIndividualRequest:
+  def test_GetIndividualRequestFactory(self):
+    request = getHSRRequest("GetBiologicalIndividual", {}, None, None)
+    assert request.getRequestType() == "GetBiologicalIndividual"
+
+  def test_GetIndividualRequest(self):
+    db = HSRDBTestImpl()
+    bi = db.newIndividual("a", "a1", 10, 30, "NA", 1)
+
+    args = {"indiv_id" : bi.indiv_id}
+    credentials = MockCredentials(True)
+
+    request = getHSRRequest("GetBiologicalIndividual", args, credentials, db)
+
+    assert request.execute()[0] == "<response>" + bi.toXml() + "</response>"
+
+  def test_GetIndividualRequestNone(self):
+    db = HSRDBTestImpl()
+    bi = db.newIndividual("a", "a1", 10, 30, "NA", 1)
+
+    args = {"indiv_id" : bi.indiv_id+2}
+    credentials = MockCredentials(True)
+
+    request = getHSRRequest("GetBiologicalIndividual", args, credentials, db)
+
+    assert request.execute()[0] == "<response></response>"
+
+  def test_GetIndividualRequestBadCred(self):
+    db = HSRDBTestImpl()
+    bi = db.newIndividual("a", "a1", 10, 30, "NA", 1)
+
+    args = {"indiv_id" : bi.indiv_id}
+    credentials = MockCredentials(False)
+
+    request = getHSRRequest("GetBiologicalIndividual", args, credentials, db)
+
+    try:
+      request.execute()[0] == "<response>" + bi.toXml() + "</response>"
+    except HSRCredentialsException, (instance):
+      assert str(instance) == "InvalidCredentials"
+    else:
+      assert False
+
+
+
+
